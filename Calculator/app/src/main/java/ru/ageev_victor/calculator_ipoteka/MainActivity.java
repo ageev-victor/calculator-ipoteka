568a0393639cd83c9179ff96877a079e945b59d7
ageev victor <fesswtra@mail.ru>
2016-10-21 15:29:45 +0300

    Добавлена стартовая активити, начата работа над главным активити

--- Calculator/app/src/main/java/ru/ageev_victor/calculator_ipoteka/MainActivity.java
+++ Calculator/app/src/main/java/ru/ageev_victor/calculator_ipoteka/MainActivity.java
@@ -3,11 +3,26 @@ package ru.ageev_victor.calculator_ipoteka;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
+
+    int stoimostKv;
+    int pervonachVznos;
+    int srokKredita;
+    double procentStavka;
+    String vidPlateza;
+    Date  nachaloViplat;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
+
+
 }
---
+++ Calculator/app/src/main/java/ru/ageev_victor/calculator_ipoteka/StartActivity.java
@@ -0,0 +1,24 @@
+package ru.ageev_victor.calculator_ipoteka;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class
+StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(StartActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 1500);
+    }
+}
